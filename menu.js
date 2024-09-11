8525aa7cb6e584e8f017129a6ad4e564eaf828f6
JaimeSCotta <jaimecotta3@gmail.com>
2024-09-11 19:06:57 +0200

    Updated

--- menu.js
+++ menu.js
@@ -1,26 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('trips.json')
+    fetch('/trips.json')
       .then(response => response.json())
       .then(data => {
-        // Ordenar por año y luego por nombre
         data.sort((a, b) => {
           if (a.year === b.year) {
             return a.name.localeCompare(b.name);
           }
-          return b.year - a.year; // Orden descendente por año
+          return b.year - a.year;
         });
-  
-        // Generar el menú
+
         const sidebar = document.querySelector('#sidebar ul');
         data.forEach(trip => {
           const li = document.createElement('li');
           const a = document.createElement('a');
-          a.href = trip.url;
+          
+          // Construir la URL correctamente
+          const baseUrl = new URL(window.location.href).origin;
+          a.href = new URL(trip.url, baseUrl).href;
+          
           a.textContent = `${trip.name}`;
           li.appendChild(a);
           sidebar.appendChild(li);
         });
       })
       .catch(error => console.error('Error al cargar el archivo JSON:', error));
-  });
-  
+});
