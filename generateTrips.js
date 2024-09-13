2d9683db8f7e39803cd9668b86f25065ea3d62e4
JaimeSCotta <jaimecotta3@gmail.com>
2024-09-13 19:59:08 +0200

    Updated

--- generateTrips.js
+++ generateTrips.js
@@ -12,12 +12,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
           // Estructura del artículo con la información del JSON
           article.innerHTML = `
-            <img src="./img/PXL_20240827_184443432.jpg" alt="${trip.name} ${trip.year}">
-            <h2>${trip.name} ${trip.year}</h2>
+            <img src="./img/img_por_defecto.jpg" alt="${trip.name}">
+            <h2>${trip.name}</h2>
             <a href="./${trip.url}" class="read-more">Read More</a>
-            <button class="fav-button" data-trip="${trip.name.replace(/\s/g, '_')}_${trip.year}" 
-                    aria-label="Añadir a favoritos ${trip.name} ${trip.year}" type="button">
-              <i class="fa-solid fa-heart"></i>
+            <button class="fav-button" data-trip="${trip.name.replace(/\s+/g, '_')}" aria-label="Añadir a favoritos ${trip.name}" type="button">
+                <i class="fa-solid fa-heart"></i>
             </button>
           `;
 
