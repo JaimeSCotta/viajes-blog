8532cff420fa81ab0a11304eed7e5c3c5042b881
JaimeSCotta <jaimecotta3@gmail.com>
2024-09-28 14:30:55 +0200

    Updated firebase logic

--- generateTrips.js
+++ generateTrips.js
@@ -1,33 +1,36 @@
+// generateTrips.js
+import { loadFavorites } from './favorites.js'; // Ajusta la ruta según la ubicación del archivo
+
 document.addEventListener('DOMContentLoaded', function () {
   fetch('/viajes-blog/trips.json') // Ruta de tu archivo trips.json
     .then(response => response.json())
     .then(tripsData => {
       generateTrips(tripsData); // Llama a la función generateTrips pasando los datos del JSON
-      loadFavorites();
+      loadFavorites(); // Asegúrate de que loadFavorites esté correctamente importada
     })
     .catch(error => console.error('Error al cargar los viajes:', error));
 });
 
 // Función para generar los viajes dinámicamente
 function generateTrips(trips) {
-const tripsContainer = document.querySelector('.main_trips');
+  const tripsContainer = document.querySelector('.main_trips');
 
-trips.forEach(trip => {
+  trips.forEach(trip => {
     // Asigna automáticamente la imagen desde la carpeta, el nombre de la imagen debe coincidir con el id
     const imageUrl = `img/trips/${trip.id}.jpg`;
 
     // Estructura HTML de cada artículo
     const tripHtml = `
-        <article>
-            <img src="${imageUrl}" alt="${trip.name}">
-            <h2>${trip.name}</h2>
-            <a href="${trip.url}" class="read-more">Read More</a>
-            <button class="fav-button" data-trip="${trip.id}" aria-label="Añadir a favoritos ${trip.name}" type="button">
-                <i class="fa-solid fa-heart"></i>
-            </button>
-        </article>`;
+      <article>
+        <img src="${imageUrl}" alt="${trip.name}">
+        <h2>${trip.name}</h2>
+        <a href="${trip.url}" class="read-more">Read More</a>
+        <button class="fav-button" data-trip="${trip.id}" aria-label="Añadir a favoritos ${trip.name}" type="button">
+          <i class="fa-solid fa-heart"></i>
+        </button>
+      </article>`;
     
     // Inserta el HTML generado en el contenedor
     tripsContainer.innerHTML += tripHtml;
-});
+  });
 }
