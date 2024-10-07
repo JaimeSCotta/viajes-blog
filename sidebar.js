80764a032ee5d58b857efeb4bb7777c5c1344a2e
JaimeSCotta <jaimecotta3@gmail.com>
2024-10-07 18:37:38 +0200

    Updated header

--- sidebar.js
+++ sidebar.js
@@ -6,11 +6,18 @@ const closeBtn = document.getElementById("closeSidebar");
 // Alternar la barra lateral al hacer clic en la hamburguesa
 openBtn.addEventListener("click", function() {
     sidebar.classList.toggle("open");
+
+    // Si la barra lateral está abierta, mostrar el botón de cerrar y ocultar el de hamburguesa
+    if (sidebar.classList.contains("open")) {
+        openBtn.style.display = "none"; // Ocultar botón hamburguesa
+        closeBtn.style.display = "block"; // Mostrar botón cerrar (X)
+    }
 });
 
 // Cerrar la barra lateral al hacer clic en la "X"
 closeBtn.addEventListener("click", function() {
     sidebar.classList.remove("open");
-});
-
-  
+    
+    openBtn.style.display = "block"; // Mostrar botón hamburguesa
+    closeBtn.style.display = "none"; // Ocultar botón cerrar (X)
+}); 
