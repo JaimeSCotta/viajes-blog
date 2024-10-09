f50eda9bff7a10d5af174990002bb3ed089ad144
JaimeSCotta <jaimecotta3@gmail.com>
2024-10-09 19:02:29 +0200

    Forgot password? updated

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 // auth.js
 import { auth, db } from './firebase.js'; // Importa auth y db desde firebase.js
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-auth.js";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-auth.js";
 import { loadFavoritesIndex } from './favorites.js'; // Importado de loadFavorites
 
 // Rutas de las imágenes
