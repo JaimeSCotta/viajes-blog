d0d156fe71ecb55662702c43d834d487753e35e0
JaimeSCotta <jaimecotta3@gmail.com>
2024-10-16 18:39:41 +0200

    Checked paths

--- menu_logic/menu_main.js
+++ menu_logic/menu_main.js
@@ -1,7 +1,7 @@
 // menu_main.js
-import { generateTrips } from '.generate_trips.js';
-import { generateMenu } from '.menu.js';
-import { setupSidebar } from '.sidebar.js'
+import { generateTrips } from './generate_trips.js';
+import { generateMenu } from './menu.js';
+import { setupSidebar } from './sidebar.js'
 
 generateTrips();
 generateMenu();
