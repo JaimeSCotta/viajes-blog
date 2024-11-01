f2a615e808beacc7d2fba6b4fd2a8ddef690b6a9
JaimeSCotta <jaimecotta3@gmail.com>
2024-11-01 17:18:59 +0100

    Testing main_favorites.js

--- main_favorites.js
+++ main_favorites.js
@@ -1,7 +1,8 @@
 // main_favorites.js
 import './auth_logic/auth_main.js';
-import './favorites_logic/favorites_main.js';
+import {loadFavorites} from './favorites_logic/favorites_main.js';
 import { generateMenu, setupSidebar } from './menu_logic/menu_main.js';
 
+loadFavorites();
 generateMenu();
 setupSidebar();
