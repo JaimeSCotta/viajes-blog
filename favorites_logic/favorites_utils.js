6b35dddae32f4e763ec68fe15c5cd90551db07c8
JaimeSCotta <jaimecotta3@gmail.com>
2024-11-01 17:36:33 +0100

    Solved removing fav form fav.html

--- favorites_logic/favorites_utils.js
+++ favorites_logic/favorites_utils.js
@@ -1,6 +1,5 @@
 // favorites_utils.js
-import { removeFavorite } from './favorites_handlers.js';
-import { loadFavorites } from './favorites_main.js';
+import { handleFavoriteToggle, loadFavorites } from './favorites_main.js';
 
 // Actualizar el estado de los botones de favoritos
 export function updateFavoriteButton(button, isFavorite) {
