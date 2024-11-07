6b5fba75b2d7718dd4ae7e0991b9b3dc0544f5da
JaimeSCotta <jaimecotta3@gmail.com>
2024-11-07 19:28:29 +0100

    Correctly located .js files

--- trips_logic/main_trips.js
+++ trips_logic/main_trips.js
@@ -1,7 +1,7 @@
 // main_trips.js
-import '/auth_logic/auth_main.js';
-import '/favorites_logic/favorites_main.js';
-import { generateMenu, setupSidebar } from '/menu_logic/menu_main.js';
+import '../auth_logic/auth_main.js';
+import '../favorites_logic/favorites_main.js';
+import { generateMenu, setupSidebar } from '../menu_logic/menu_main.js';
 
 generateMenu();
 setupSidebar();
