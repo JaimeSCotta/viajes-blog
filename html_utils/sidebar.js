0ed5fb27af95c225fe16a64e39623afcf1879937
JaimeSCotta <jaimecotta3@gmail.com>
2024-11-11 17:17:26 +0100

    Sidebar debugged

--- html_utils/sidebar.js
+++ html_utils/sidebar.js
@@ -1,20 +1,22 @@
 // sidebar.js
-
-import { generateMenu } from '../menu_logic/menu_main.js';
-
-document.addEventListener("DOMContentLoaded", function() {
-  const sidebar = `
-    <!-- Barra lateral izquierda con todos los viajes -->
-    <div id="sidebar" class="sidebar">
-      <button class="hamburger" id="sidebarHamburger" aria-label="Abrir barra lateral" type="button">
-        <i class="fas fa-bars"></i>
-      </button>
-      <button id="closeSidebar" class="close-btn" type="button">&times;</button>
-      <ul>
-        <!-- Contenido de la barra lateral -->
-      </ul>
-    </div>
-  `;
-  document.getElementById("sidebar-placeholder").innerHTML = sidebar;
-  generateMenu();
-});
+export function loadSidebar() {
+  return new Promise((resolve) => {
+    document.addEventListener("DOMContentLoaded", function() {
+      const sidebar = `
+        <div id="sidebar" class="sidebar">
+          <button class="hamburger" id="sidebarHamburger" aria-label="Abrir barra lateral" type="button">
+            <i class="fas fa-bars"></i>
+          </button>
+          <button id="closeSidebar" class="close-btn" type="button">&times;</button>
+          <ul>
+            <!-- Contenido de la barra lateral se cargará aquí -->
+          </ul>
+        </div>
+      `;
+      document.getElementById("sidebar-placeholder").innerHTML = sidebar;
+      
+      // Resuelve la promesa después de que el sidebar esté en el DOM
+      resolve();
+    });
+  });
+}
--- trips_logic/main_trips.js
+++ trips_logic/main_trips.js
@@ -1,8 +1,11 @@
 // main_trips.js
 import '../auth_logic/auth_main.js';
 import '../favorites_logic/favorites_main.js';
-import { setupSidebar } from '../menu_logic/menu_main.js';
+import { loadSidebar } from '../html_utils/sidebar.js';
+import { generateMenu, setupSidebar } from '../menu_logic/menu_main.js';
 
-document.addEventListener("DOMContentLoaded", function() {
-    setupSidebar();
+document.addEventListener("DOMContentLoaded", async function() {
+    await loadSidebar();  // Espera a que el sidebar se cargue
+    generateMenu();       // Carga el menú una vez que el sidebar esté en el DOM
+    setupSidebar();       // Configura los eventos del sidebar
 });
