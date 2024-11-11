a8f10d6c3e97e1b7b8d17e16cb6c30c050896914
JaimeSCotta <jaimecotta3@gmail.com>
2024-11-11 16:55:39 +0100

    Wait for DOM

--- trips_logic/main_trips.js
+++ trips_logic/main_trips.js
@@ -3,5 +3,7 @@ import '../auth_logic/auth_main.js';
 import '../favorites_logic/favorites_main.js';
 import { generateMenu, setupSidebar } from '../menu_logic/menu_main.js';
 
-generateMenu();
-setupSidebar();
+document.addEventListener("DOMContentLoaded", function() {
+    generateMenu();
+    setupSidebar();
+});
